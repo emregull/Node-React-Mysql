be9b665008d9ec60041b262255e6af08f9aa9a87
Emre Gül <eoeemre@hotmail.com>
2022-07-26 14:04:41 +0300

    Level 8
    Login - Register System

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router ,Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Post  from './pages/Post';
+import Login  from './pages/Login';
+import Registration  from './pages/Registration';
+
 
 function App() {
 
@@ -13,11 +16,15 @@ function App() {
         <div className="navbar">
           <Link to="/createpost">Create A Post</Link>
           <Link to="/">Home Page</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/registration">Register</Link>
           </div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/createpost" element={<CreatePost />} />
             <Route path="/post/:id" element={<Post />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
           </Routes>
        
         </Router>
